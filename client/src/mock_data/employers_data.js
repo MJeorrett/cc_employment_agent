71c059393ff60f2fa9ce170425da66bda399829e
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-21 10:35:45 +0000

    fixing error missing commas and duplicate id

--- client/src/mock_data/employers_data.js
+++ client/src/mock_data/employers_data.js
@@ -1,9 +1,9 @@
-const employers = [
+const employers_data = [
   {
-    id: 1
+    id: 1,
     company_name: "FreeAgent",
     company_website: "www.freeagent.com",
-    contact_details: "David Jones, 077044999333"
+    contact_details: "David Jones, 077044999333",
     company_logo_url: "https://www.freeagent.com/components/images/company/branding/freeagent-logo-7c2816e6.png",
     type: "product",
     address: "One Edinburgh Quay, 133 Fountainbridge, Edinburgh, EH3 9QG",
@@ -11,10 +11,10 @@ const employers = [
     notes: "moving office next week"
   },
   {
-    id: 2
+    id: 2,
     company_name: "FanDuel",
     company_website: "www.fanduel.com",
-    contact_details: "Robin Spira, 0770111445"
+    contact_details: "Robin Spira, 0770111445",
     company_logo_url: "https://s3-us-west-1.amazonaws.com/dfc-production/uploads/26/db3127aa455ad0d901a709c02701e8b5.jpeg",
     type: "product",
     address: "Quartermile, 1 Laurieston Place",
@@ -22,10 +22,10 @@ const employers = [
     notes: "Let them know about next speed networking event"
   },
   {
-    id: 3
+    id: 3,
     company_name: "Bemo",
     company_website: "http://bemo.co/",
-    contact_details: "Derek Young, 07704455533"
+    contact_details: "Derek Young, 07704455533",
     company_logo_url: "https://static1.squarespace.com/static/55439320e4b0f92b5d6c4c8b/t/583449111b631beda5883280/1479821621987/?format=500w",
     type: "mobile consultant",
     address: "CODEBASE, ARGYLE HOUSE, 38 CASTLE TERRACE, EDINBURGH, EH3 9DS",
@@ -33,10 +33,10 @@ const employers = [
     notes: "Looking to hire from CodeClan soon"
   },
   {
-    id: 4
+    id: 4,
     company_name: "RBS",
     company_website: "http://www.rbs.com/",
-    contact_details: "David Jones, 077044999333"
+    contact_details: "David Jones, 077044999333",
     company_logo_url: "https://cdn.evbuc.com/eventlogos/180898321/royalbankofscotlandlogo.jpg",
     type: "corporate",
     address: "One Edinburgh Quay, 133 Fountainbridge, Edinburgh, EH3 9QG",
@@ -44,10 +44,10 @@ const employers = [
     notes: "Keen to hire from CodeClan"
   },
   {
-    id: 5
+    id: 5,
     company_name: "Adobe",
     company_website: "http://www.adobe.com/uk/",
-    contact_details: "Robin Spira, 0770111445"
+    contact_details: "Robin Spira, 0770111445",
     company_logo_url: "http://mobileadvertisingwatch.com/wp-content/uploads/2015/08/The-New-Code-is-No-Code-Adobe-Digital-Publishing-Solution-Delivers-Content-to-Mobile-Apps.jpg",
     type: "multinational",
     address: "Quartermile, 1 Laurieston Place",
@@ -55,10 +55,10 @@ const employers = [
     notes: "Let them know about next speed networking event"
   },
   {
-    id: 3
+    id: 6,
     company_name: "Beezer",
     company_website: "https://www.beezer.com/",
-    contact_details: "Derek Young, 07704455533"
+    contact_details: "Derek Young, 07704455533",
     company_logo_url: "https://www.weareninetwenty.com/userfiles/NineTwentyMain/WebContent/beezer_logo_FIN2-01.png",
     type: "product",
     address: "CODEBASE, ARGYLE HOUSE, 38 CASTLE TERRACE, EDINBURGH, EH3 9DS",
@@ -67,4 +67,4 @@ const employers = [
   },
 ]
 
-export default employers
+export default employers_data
