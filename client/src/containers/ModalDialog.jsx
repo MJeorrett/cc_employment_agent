f77ac26f4b941c682413a25807de44f06ae9f755
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-21 11:37:02 +0000

    adding styling to modal dialgoue

--- client/src/containers/ModalDialog.jsx
+++ client/src/containers/ModalDialog.jsx
@@ -4,7 +4,15 @@ class ModalDialog extends React.Component {
 
   render() {
     return (
-      <div id="modal-dialog">This is a modal dialog</div>
+      <div id="modal-dialog">
+        <div id="modal-dialogue-title-bar">
+          <h2>Modal Dialog Title</h2>
+          <span id="modal-dialog-close-button">X</span>
+          <div id="modal-dialog-content">
+            { this.props.children }
+          </div>
+        </div>
+      </div>
     )
   }
 
