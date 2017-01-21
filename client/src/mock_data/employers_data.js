3c9d3eb9a9d987cdcf3fda5d3591ae01aacd31fb
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-21 10:27:20 +0000

    adding employers seed data

---
+++ client/src/mock_data/employers_data.js
@@ -0,0 +1,70 @@
+const employers = [
+  {
+    id: 1
+    company_name: "FreeAgent",
+    company_website: "www.freeagent.com",
+    contact_details: "David Jones, 077044999333"
+    company_logo_url: "https://www.freeagent.com/components/images/company/branding/freeagent-logo-7c2816e6.png",
+    type: "product",
+    address: "One Edinburgh Quay, 133 Fountainbridge, Edinburgh, EH3 9QG",
+    no_previous_students_hired: 2,
+    notes: "moving office next week"
+  },
+  {
+    id: 2
+    company_name: "FanDuel",
+    company_website: "www.fanduel.com",
+    contact_details: "Robin Spira, 0770111445"
+    company_logo_url: "https://s3-us-west-1.amazonaws.com/dfc-production/uploads/26/db3127aa455ad0d901a709c02701e8b5.jpeg",
+    type: "product",
+    address: "Quartermile, 1 Laurieston Place",
+    no_previous_students_hired: 0,
+    notes: "Let them know about next speed networking event"
+  },
+  {
+    id: 3
+    company_name: "Bemo",
+    company_website: "http://bemo.co/",
+    contact_details: "Derek Young, 07704455533"
+    company_logo_url: "https://static1.squarespace.com/static/55439320e4b0f92b5d6c4c8b/t/583449111b631beda5883280/1479821621987/?format=500w",
+    type: "mobile consultant",
+    address: "CODEBASE, ARGYLE HOUSE, 38 CASTLE TERRACE, EDINBURGH, EH3 9DS",
+    no_previous_students_hired: 1,
+    notes: "Looking to hire from CodeClan soon"
+  },
+  {
+    id: 4
+    company_name: "RBS",
+    company_website: "http://www.rbs.com/",
+    contact_details: "David Jones, 077044999333"
+    company_logo_url: "https://cdn.evbuc.com/eventlogos/180898321/royalbankofscotlandlogo.jpg",
+    type: "corporate",
+    address: "One Edinburgh Quay, 133 Fountainbridge, Edinburgh, EH3 9QG",
+    no_previous_students_hired: 0,
+    notes: "Keen to hire from CodeClan"
+  },
+  {
+    id: 5
+    company_name: "Adobe",
+    company_website: "http://www.adobe.com/uk/",
+    contact_details: "Robin Spira, 0770111445"
+    company_logo_url: "http://mobileadvertisingwatch.com/wp-content/uploads/2015/08/The-New-Code-is-No-Code-Adobe-Digital-Publishing-Solution-Delivers-Content-to-Mobile-Apps.jpg",
+    type: "multinational",
+    address: "Quartermile, 1 Laurieston Place",
+    no_previous_students_hired: 6,
+    notes: "Let them know about next speed networking event"
+  },
+  {
+    id: 3
+    company_name: "Beezer",
+    company_website: "https://www.beezer.com/",
+    contact_details: "Derek Young, 07704455533"
+    company_logo_url: "https://www.weareninetwenty.com/userfiles/NineTwentyMain/WebContent/beezer_logo_FIN2-01.png",
+    type: "product",
+    address: "CODEBASE, ARGYLE HOUSE, 38 CASTLE TERRACE, EDINBURGH, EH3 9DS",
+    no_previous_students_hired: 1,
+    notes: "Looking to hire from CodeClan soon"
+  },
+]
+
+export default employers
