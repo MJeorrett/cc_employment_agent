3bbf449d0b9692f0a68a1c319ff41462bc3dbddd
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-21 09:15:17 +0000

    adding inputs for email and password to login page

--- client/src/LoginContainer.jsx
+++ client/src/LoginContainer.jsx
@@ -4,18 +4,48 @@ class LoginContainer extends React.Component {
 
   constructor() {
     super()
+    this.state = {
+      email: "",
+      password: ""
+    }
+    this.handleEmailChange = this.handleEmailChange.bind( this )
+    this.handlePasswordChange = this.handlePasswordChange.bind( this )
     this.handleLogInClicked = this.handleLogInClicked.bind( this )
   }
 
+  handleEmailChange( ev ) {
+    this.setState({
+      email: ev.target.value
+    })
+  }
+
+  handlePasswordChange( ev ) {
+    this.setState({
+      password: ev.target.value
+    })
+  }
+
   handleLogInClicked() {
-    this.props.onLogIn( "Matthew Jeorrett" )
+    this.props.onLogIn( this.state.email )
   }
 
   render() {
     return (
       <div>
         <h2>Log In</h2>
-        <button onClick={ this.handleLogInClicked }>Sign In</button>
+        <input
+          type="text"
+          onChange={ this.handleEmailChange }
+          value={ this.state.email }
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          onChange={ this.handlePasswordChange }
+          value={ this.state.password }
+          placeholder="Password"
+        />
+        <button onClick={ this.handleLogInClicked }>Log In</button>
       </div>
     )
   }
