0152204440bdfb4d63b32664a40428e0246c8adb
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-25 10:19:11 +0000

    populating jobs from api instead of mock data

--- client/src/containers/JobsContainer.jsx
+++ client/src/containers/JobsContainer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-import jobsData from '../mock_data/jobs_data'
 import Table from './Table'
 
 class JobsContainer extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      jobsData = []
+    }
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:5000/api/jobs'
+    XmlHttpHelper.get( url, (jobsData) => {
+      this.setState({
+        jobsData: jobsData
+      })
+    })
+  }
 
   render() {
     const columnNames = [
@@ -15,7 +30,7 @@ class JobsContainer extends React.Component {
     ]
     return (
       <Table
-        data={ jobsData }
+        data={ this.state.jobsData }
         columnNamesData={ columnNames }
       />
     )
